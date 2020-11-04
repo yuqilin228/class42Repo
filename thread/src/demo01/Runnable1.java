4a9f99e987906c7327fac7a275bf384e4ef7d6ba
ljt <1481595562@qq.com>
2020-11-04 11:06:04 +0800

    代码提交

---
+++ thread/src/demo01/Runnable1.java
@@ -0,0 +1,9 @@
+package demo01;
+
+public class Runnable1 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("任务类开始执行了...");
+	}
+}
---
+++ thread/src/demo01/Test.java
@@ -0,0 +1,15 @@
+package demo01;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class Test {
+	public static void main(String[] args) {
+		/*Thread t1 = new MyThread();
+		Thread t2 = new MyThread();
+		t1.start();
+		t2.start();*/
+		
+		Thread t3 = new Thread(new Runnable1());
+		t3.start();
+	}
+}
